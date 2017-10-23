d5e1b8b1728e524e163afdaf6a350f3be9de7a6f
matt curry <mc584@afsconnect2.njit.edu>
2017-10-23 17:56:40 -0400

    added file

---
+++ backup.php
@@ -0,0 +1,37 @@
+<?php
+
+echo "<h1>PDO demo!</h1>";
+
+$hostname = "sql2.njit.edu";
+$username = "mc584";
+$password = "bwf4TMcD";
+try {
+	    $conn = new PDO("mysql:host=$hostname;dbname=mc584",
+	    $username, $password);
+	    echo "Connected successfully<br>"; 
+	    $sql = "select * from accounts where id < 6";
+	    $q = $conn->prepare($sql);
+	    $q->execute();
+	    $results = $q->fetchAll();
+	    
+
+	    if($q->rowCount()){
+	    	echo count($results);
+
+
+	    	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Pass</th></tr>";
+	    	foreach ($results as $row) {
+        		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["gender"]."</td><td>".$row["password"]."</td></tr>";
+    		}
+	    }else{
+	    	echo '0 results';
+	    } 
+
+    }
+catch(PDOException $e)
+    {
+    	echo "Connection failed: " . $e->getMessage();
+    }
+$conn = null;
+
+?>
